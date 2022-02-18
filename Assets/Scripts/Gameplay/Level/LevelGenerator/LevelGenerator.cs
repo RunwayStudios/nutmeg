651cc4ff7544b9470023ec0be8e9cce597ae19ca
Kdender <51720989+Kdender@users.noreply.github.com>
2022-02-18 01:46:40 +0100

    yes

--- Assets/Scripts/Gameplay/Level/LevelGenerator/LevelGenerator.cs
+++ Assets/Scripts/Gameplay/Level/LevelGenerator/LevelGenerator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Timers;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -19,6 +21,12 @@ namespace Gameplay.Level.LevelGenerator
         [SerializeField] private float noiseScale = 1f;
         [SerializeField] private float noiseMultiplier = 1f;
 
+
+        [SerializeField] private bool regenerate = false;
+        private Stopwatch terrainGenStopwatch = new Stopwatch();
+        [SerializeField] private double generateMS = 0f;
+
+
         private void Awake()
         {
         }
@@ -32,7 +40,14 @@ namespace Gameplay.Level.LevelGenerator
         // Update is called once per frame
         void Update()
         {
-            
+            if (regenerate)
+            {
+                regenerate = false;
+                terrainGenStopwatch.Restart();
+                GenerateLevel();
+                terrainGenStopwatch.Stop();
+                generateMS = terrainGenStopwatch.Elapsed.TotalMilliseconds;
+            }
         }
 
 
@@ -95,9 +110,10 @@ namespace Gameplay.Level.LevelGenerator
                         triangles[triangleIndex++] = (ushort)(vertIndex + 1);
                     }
 
-                    float terrainYOffset = Mathf.PerlinNoise((x + 0.1f) * noiseScale, (z + 0.1f) * noiseScale) * noiseMultiplier;
+                    float terrainYOffset = Mathf.PerlinNoise((x + 0.1f) * noiseScale, (z + 0.1f) * noiseScale) *
+                                           noiseMultiplier;
 
-                    verts[vertIndex++] = new TerrainVertex(new Vector3(x, 10f + terrainYOffset, z), 0, 1);
+                    verts[vertIndex++] = new TerrainVertex(new Vector3(x, terrainYOffset, z), 0, 1);
                 }
             }
 
@@ -109,67 +125,6 @@ namespace Gameplay.Level.LevelGenerator
             mesh.RecalculateBounds();
 
             GetComponent<MeshFilter>().mesh = mesh;
-
-
-            // var mesh = new Mesh();
-            // // specify vertex count and layout
-            // var layout = new[]
-            // {
-            //     new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
-            //     new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float16, 2),
-            //     //new VertexAttributeDescriptor(VertexAttribute.Tangent, VertexAttributeFormat.UNorm8, 4),
-            // };
-            // var vertexCount = 4;
-            // mesh.SetVertexBufferParams(vertexCount, layout);
-            //
-            // // set vertex data
-            // var verts = new NativeArray<TerrainVertex>(vertexCount, Allocator.Temp,
-            //     NativeArrayOptions.UninitializedMemory);
-            //
-            // verts[0] = new TerrainVertex(new Vector3(0f, 10f, 0f), 0, 1);
-            // verts[1] = new TerrainVertex(new Vector3(0f, 10f, 10f), 0, 1);
-            // verts[2] = new TerrainVertex(new Vector3(10f, 10f, 0f), 0, 1);
-            // verts[3] = new TerrainVertex(new Vector3(10f, 10f, 10f), 0, 1);
-            //
-            //
-            // var triangles = new NativeArray<ushort>(6, Allocator.Temp,
-            //     NativeArrayOptions.UninitializedMemory);
-            //
-            // triangles[0] = 0;
-            // triangles[1] = 1;
-            // triangles[2] = 2;
-            // triangles[3] = 3;
-            // triangles[4] = 2;
-            // triangles[5] = 1;
-            //
-            // //mesh.indexFormat = IndexFormat.UInt16;
-            //
-            // mesh.SetVertexBufferData(verts, 0, 0, vertexCount, 0, MeshUpdateFlags.Default);
-            //
-            // //mesh.SetIndexBufferParams(6, IndexFormat.UInt16);
-            // //mesh.SetIndexBufferData(triangles, 0, 0, 6, MeshUpdateFlags.Default);
-            // mesh.SetIndices(triangles, MeshTopology.Triangles, 0, false, 0);
-            // //mesh.SetTriangles(triangles.ToArray(), 0, false);
-            //
-            //
-            // //mesh.RecalculateNormals();
-            // mesh.RecalculateBounds();
-            //
-            //
-            // // Debug.Log("Indices: " + mesh.GetIndices(0));
-            // // foreach (var index in mesh.GetIndices(0))
-            // // {
-            // //     Debug.Log(index);
-            // // }
-            // //
-            // // Debug.Log("Triangles: " + mesh.GetTriangles(0));
-            // // foreach (var index in mesh.GetTriangles(0))
-            // // {
-            // //     Debug.Log(index);
-            // // }
-            //
-            //
-            // GetComponent<MeshFilter>().mesh = mesh;
         }
     }
 }
